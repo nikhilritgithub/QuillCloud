5dc438e638ab11c326a782b87aa1b6075b548047
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-04-29 12:45:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.post("/edit-userName", function(req ,res){
         res.redirect("/created");
     }
 })
+
 app.post('/create', function(req, res){
     fs.writeFile(`./files/${req.body.title.split(' ').join(' ')}.txt`, req.body.details, function(err){
         res.redirect("/");
