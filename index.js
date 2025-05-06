eae32effe49592574091486ccc6a109e53a1cdf1
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-05-06 19:39:14 +0530

    Update index.js
    added new users.

--- index.js
+++ index.js
@@ -43,5 +43,10 @@ app.post('/create', function(req, res){
     });
 })
 
-    
+
+app.post("/registered-user", function(req, res){
+    fs.ReadFile(`./files/new/`, req.body.details, function(err){
+        res.redirect("/created");
+    })
+})
 app.listen(3000);
