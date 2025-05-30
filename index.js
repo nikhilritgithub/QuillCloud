813d75a4bb613da6396904978d8babb4ea144d49
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-05-30 22:19:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.get('/file/:filename', function(req, res){
 app.get('/edit/:filename',function(req, res){
    res.render('edit',{filename: req.params.filename});
 })
+
 app.post('/edit', function(req, res){
     fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`, function(err){
         res.redirect("/");
