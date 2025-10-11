df53fb7165ec6f75cf4a41bebf256fc5d6aa2179
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-10-11 20:58:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
-//use for linking css file and vanila javascript.
-
+//use for linking css file and vanila JS.
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', function(req, res){
