3e512b53ac6589cd349f4b30a4674e834288b50c
Nikhil kumar <142686565+nikhilritgithub@users.noreply.github.com>
2025-10-11 20:57:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ app.post('/create', function(req, res){
         res.redirect("/");
     });
 })
+
 app.post("/registered-user", function(req, res){
     fs.ReadFile(`./files/new/`, req.body.details, function(err){
         res.redirect("/created");
